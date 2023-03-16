14c176255659cf5261387cbb5486fea8d8de3123
Karl Veggerby <90000892+veggerk@users.noreply.github.com>
2023-03-15 18:03:40 -0700

    Add files via upload

---
+++ R/source_anovas_and_plots.R
@@ -0,0 +1,141 @@
+
+# setup ---------------------------------------------------------------
+library(tidyverse)
+library(agricolae)
+library(here)
+
+
+walleye_prey<-"prey_data_walleye.csv"
+npm_prey<-"prey_data_npm.csv"
+
+prey<-"prey_data.csv"
+preds<-"predator_data.csv"
+
+
+## raw file locations
+walleye_prey<- here("analysis_and_clean_data", walleye_prey)
+npm_prey<- here("analysis_and_clean_data", npm_prey)
+prey<- here("analysis_and_clean_data", prey)
+preds<- here("analysis_and_clean_data", preds)
+
+## pull in files from sub directory
+walleye_prey <- read_csv(walleye_prey,
+                  na = c("", "NA"))
+
+npm_prey <- read_csv(npm_prey,
+                         na = c("", "NA"))
+
+prey <- read_csv(prey,
+                         na = c("", "NA"))
+
+preds <- read_csv(preds,
+                         na = c("", "NA"))
+
+
+
+colnames(preds)[which(names(preds) == "Identifier 2")] <- "predator"
+colnames(prey)[which(names(prey) == "Identifier 2")] <- "Sources"
+colnames(npm_prey)[which(names(npm_prey) == "Identifier 2")] <- "Sources"
+colnames(walleye_prey)[which(names(walleye_prey) == "Identifier 2")] <- "Sources"
+
+# summary of data collection -----------------------------------------------
+
+counts_prey<-prey%>% count(Sources, sort = TRUE)
+
+counts_pred<-preds%>% count(predator, sort = TRUE)
+
+prey_all<-prey
+
+
+prey<-prey[!(prey$Sources=="3 Spine Stickleback"|
+               prey$Sources=="Catfish"|
+             prey$Sources=="Hatchery sockeye"|
+  prey$Sources=="Lamprey"|
+  prey$Sources=="Perch"|
+  prey$Sources=="Sunfish"|
+  prey$Sources=="Whitefish"|
+  prey$Sources=="Wild coho"|
+  prey$Sources=="Wild sockeye"),]
+
+
+
+#prey ANOVAS for possible prey species, 
+# species deemed to not be prey items excluded
+
+#run ANOVA 
+
+
+avnitrogen <- aov(formula = d15N ~ Sources,
+                  data = prey)
+#summarize ANOVA 
+summary(avnitrogen)
+#run Tukey test 
+tukeynitrogen<-HSD.test(y=avnitrogen,trt = "Sources")
+tukeynitrogen
+
+
+#run ANOVA 
+avcarbon <- aov(formula = d13C ~ Sources,
+                  data = prey)
+#summarize ANOVA 
+summary(avcarbon)
+#run Tukey test 
+tukeycarbon<-HSD.test(y=avcarbon,trt = "Sources")
+tukeycarbon
+
+# some quick plots to look at the prey groupings, starting with everything, subset down to prey 
+# to include in model, to grouped prey categories. crayfish are included for NPM, 
+# as they were a common prey item from the prior: Poe et al. 
+
+
+
+prey_all<-prey_all[!(prey_all$Sources=="Lamprey"),]
+
+
+avg_prey_all<-prey_all%>% 
+  group_by(Sources)%>%
+  summarise(mean(d13C), mean(d15N))
+
+colnames(avg_prey_all)[which(names(avg_prey_all) == "mean(d13C)")] <- "d13C"
+colnames(avg_prey_all)[which(names(avg_prey_all) == "mean(d15N)")] <- "d15N"
+
+
+ggplot(data=avg_prey_all, aes(x=d13C, y=d15N, colour=Sources))+
+  geom_point()
+
+
+
+
+
+avg_prey<-prey%>% 
+  group_by(Sources)%>%
+  summarise(mean(d13C), mean(d15N))
+
+colnames(avg_prey)[which(names(avg_prey) == "mean(d13C)")] <- "d13C"
+colnames(avg_prey)[which(names(avg_prey) == "mean(d15N)")] <- "d15N"
+
+
+ggplot(data=avg_prey, aes(x=d13C, y=d15N, colour=Sources))+
+  geom_point()
+
+
+
+grouped_names<-c("American shad",
+                 "mixed",
+                 "mixed",
+                 "yearling",
+                 "subyearling",
+                 "yearling",
+                 "mixed",
+                 "mixed",
+                 "mixed",
+                 "yearling",
+                 "subyearling",
+                 "yearling")
+
+avg_prey_grouped<-cbind(avg_prey,grouped_names)
+
+ggplot(data=avg_prey_grouped, aes(x=d13C, y=d15N, colour=grouped_names))+
+  geom_point()
+
+
---
+++ README.md
@@ -0,0 +1,44 @@
+# walleye-SI-project
+
+This repository is a scientific product and is not official communication of the National Oceanic and Atmospheric Administration, or the United States Department of Commerce. All NOAA GitHub project code is provided on an ‘as is’ basis and the user assumes responsibility for its use. Any claims against the Department of Commerce or Department of Commerce bureaus stemming from the use of this GitHub project will be governed by all applicable Federal law. Any reference to specific commercial products, processes, or services by service mark, trademark, manufacturer, or otherwise, does not constitute or imply their endorsement, recommendation or favoring by the Department of Commerce. The Department of Commerce seal and logo, or the seal and logo of a DOC bureau, shall not be used in any manner to imply endorsement of any commercial product or activity by DOC or the United States Government.
+
+
+
+This repository has the following structure: 
+
+`\R` folder containing R code
+
+`\R\map-making-and-figure-merging-for-walleye` folder containing R code and files for making figure 1, and merging individual MixSIAR output plots into multiplots for other figures.
+
+`\data\raw data` folder with raw data files
+
+`\data\clean_data` folder with cleaned data files
+
+`\figs` folder with figures
+ 
+`\model output` folder with isospace plots for walleye and northern pikeminnow, and model output and diagnostics for each model. Each model has its own folder within the 'model output' folder.
+
+metadata describing the column names is provided in: 'metadata for spreadsheets.xlsx'
+
+The MixSIAR user manual is: 'MixSIAR_user_manual.pdf'
+
+
+
+General notes: 
+
+The plots I generated from model results were modified from the standard MisSIAR output, which while nice generally isn't publication ready on it's own. Within the code for the output_JAGS function I modified the ggplot2 code to change how the model generated posterior density plots.
+
+MixSIAR can use either averaged prey data, or raw prey data. It makes no difference to the model which you use, as the data is averaged by MixSIAR when it's imported into R anyway. I have both the raw and average prey data in the clean data folder. 
+
+It takes anywhere from a few hours to a few days to run a single model, depending on the model size and the computer you are using.  
+
+The markdown files in the 'R' folder contain the models for both walleye and northern pikeminnow. I separated the models with different effects into different markdown docs to make things more organized. 
+
+The 'walleye_npm_MixSIAR.Rmd' file contains the mixing models for walleye and northern pikeminnow with no added effects. 
+
+The 'walleye_npm_season_effect.Rmd' file contains the mixing models for walleye and northern pikeminnow with season/migration timing effect added. 
+
+The 'walleye_npm_size_effect.Rmd' file contains the mixing models for walleye and northern pikeminnow with a continuous size effect added. (note the walleye model failed to converge) 
+
+The 'walleye_size_effect_by_group.Rmd' file contains the mixing model for walleye with a fixed size effect added in lieu of a successful continuous size effect. 
+
